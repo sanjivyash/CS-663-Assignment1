902467e53f1bbf18c899a9493d7aad227e9b4cee
sanjivyash <sanjiv.yash@gmail.com>
2020-08-27 00:52:49 +0530

    histogram matching

--- 2/code/myMainScript.py
+++ 2/code/myMainScript.py
@@ -13,6 +13,23 @@ def extractImage(path):
 	return img
 
 
+def presums(arr):
+	ans = arr.copy()
+	for i in range(1, len(arr)):
+		ans[i] = arr[i] + ans[i-1]
+	return ans 
+
+
+def invert(arr):
+	ans = [0 for i in range(256)]
+	arr = arr.astype(np.int32)
+	i = 0
+	for j in range(256):
+		ans[i:arr[j] + 1] = [j]*(arr[j] + 1 - i)
+		i = arr[j] + 1
+	return ans
+
+
 def myForegroundMask(path):
 	if isinstance(path, str):
 		img = extractImage(path)
@@ -85,20 +102,102 @@ def myLinearContrastStretching(path):
 	plt.show()
 
 
+def myHE(path):
+	if isinstance(path, str):
+		img = extractImage(path)
+	elif isinstance(path, np.ndarray):
+		img = path
+	else:
+		raise TypeError('Provide image or path')
+
+	m, n = img.shape[:2]
+	R = [0 for i in range(256)]
+	G, B = R.copy(), R.copy()
+
+	for i in range(m):
+		for j in range(n):
+			r, g, b = img[i][j]
+			R[r] += 1
+			G[g] += 1
+			B[b] += 1
+
+	sR, sG, sB = presums(R), presums(G), presums(B)
+	fR, fG, fB = [round(255*i/(m*n)) for i in sR], [round(255*i/(m*n)) for i in sG], [round(255*i/(m*n)) for i in sB]
+
+	out = np.zeros(img.shape, dtype=np.uint8)
+
+	for i in range(m):
+		for j in range(n):
+			r, g, b = img[i][j]
+			out[i][j] = np.array([fR[r], fG[g], fB[b]])
+
+	plt.figure()
+	plt.imshow(img)
+
+	plt.figure()
+	plt.imshow(out)
+
+	plt.show()
+
+
+def myHM():
+	img = extractImage("../data/retina.png")
+	ideal = extractImage("../data/retinaRef.png")
+
+	px_img = np.zeros((3,256), dtype=np.int32)
+	px_ideal = 	np.zeros((3,256), dtype=np.int32)
+
+	m, n = img.shape[:2]
+
+	for i in range(m):
+		for j in range(n):
+			for k in range(3):
+				px_img[k][img[i][j][k]] += 1
+				px_ideal[k][ideal[i][j][k]] += 1
+
+	cdf_img = np.array([np.round(presums(row)*255/(m*n)) for row in px_img])
+	cdf_ideal = np.array([np.round(presums(row)*255/(m*n)) for row in px_ideal])
+	inv_ideal = np.array([invert(row) for row in cdf_ideal])
+
+	out = np.zeros(img.shape, dtype=np.uint8)
+
+	for i in range(m):
+		for j in range(n):
+			for k in range(3):
+				out[i][j][k] = inv_ideal[k][int(cdf_img[k][img[i][j][k]])]
+
+	plt.figure()
+	plt.imshow(img)
+
+	plt.figure()
+	plt.imshow(ideal)
+
+	plt.figure()
+	plt.imshow(out)
+
+	plt.show()
+
+
 if __name__ == '__main__':
 	IMG_DIR = os.path.join(os.path.dirname(__file__), '..', 'data')
 	
 	path = os.path.join(IMG_DIR, 'statue.png')
 	out = myForegroundMask(path)
-	# myLinearContrastStretching(out)
 	
-	# path = os.path.join(IMG_DIR, 'retina.png')
-	# myLinearContrastStretching(path)
+	myLinearContrastStretching(out)
+	myHE(out)
+	
+	myHM()
 
-	# for file in os.listdir(IMG_DIR):
-	# 	if 'statue' in file or 'retina' in file:
-	# 		continue 
+	path = os.path.join(IMG_DIR, 'retina.png')
+	myLinearContrastStretching(path)
+	myHE(path)
 
-	# 	print(file)
-	# 	path = os.path.join(IMG_DIR, file)
-	# 	myLinearContrastStretching(path)
+	for file in os.listdir(IMG_DIR):
+		if 'statue' in file or 'retina' in file:
+			continue 
+
+		print(file)
+		path = os.path.join(IMG_DIR, file)
+		myLinearContrastStretching(path)
+		myHE(path)
