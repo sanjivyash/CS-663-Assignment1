f931f56ad4a9aef3f012d55ce07ac51a03702c97
sanjivyash <sanjiv.yash@gmail.com>
2020-09-01 15:18:56 +0530

    Q3 code

--- 2/code/myCLAHE.py
+++ 2/code/myCLAHE.py
@@ -22,6 +22,41 @@ def presums(arr):
   return ans 
 
 
+def get_cdf(image, threshold, N):
+  m, n = image.shape[:2]
+  
+  R = np.zeros(256, dtype=np.float64)
+  G, B = R.copy(), R.copy()
+
+  for i in range(m):
+    for j in range(n):
+      r, g, b = image[i][j]
+      R[r] += 1
+      G[g] += 1
+      B[b] += 1
+  
+  R, G, B = (R/(N*N)), (G/(N*N)), (B/(N*N)) 
+  sum_r, sum_g, sum_b = np.zeros(3, dtype=np.float64)
+  
+  for i in range(256):
+    if(R[i] > threshold):
+      sum_r += R[i]-threshold
+      R[i] = threshold
+    
+    if(G[i] > threshold):
+      sum_g += G[i]-threshold
+      G[i] = threshold
+    
+    if(B[i] > threshold):
+      sum_b += B[i]-threshold
+      B[i] = threshold
+  
+  R, G, B = (R + (sum_r/256)), (G + (sum_g/256)), (B + (sum_b/256))
+  sR, sG, sB = presums(R), presums(G), presums(B)
+
+  return sR, sG, sB
+
+
 def gen_windows(img, N, threshold, m, n):
   for i in range(N//2, (m+N//2)):
     for j in range(N//2, (n+N//2)):
@@ -68,7 +103,92 @@ def make_hist(tup):
   return (np.array([sR[r1], sG[g1], sB[b1]]), (i1,j1))
 
 
-def myCLAHE(path, N, threshold):
+def myBilinearCLAHE(path, N, threshold):
+  if(isinstance(path, str)):
+    img = extractImage(path)
+  elif(isinstance(path, np.ndarray)):
+    img = path
+  else:
+    raise TypeError('Enter path or image')
+
+  m,n = img.shape[:2]
+  out = np.zeros(img.shape, dtype=np.float64)
+
+  cdf_m = (m//N) + (1 if (m%N!=0) else 0)
+  cdf_n = (n//N) + (1 if (n%N!=0) else 0)
+  cdfs = np.ndarray((cdf_m, cdf_n, 3, 256), dtype=np.float64)
+ 
+  for i in range(cdf_m):
+        for j in range(cdf_n):
+            cdfs[i][j] = get_cdf(img[i:(i+N), j:(j+N)], threshold, N)
+  
+  for i in range(m):
+    for j in range(n):
+      
+      if(i<(N//2) or (j<(N//2))):
+        fR, fG, fB = cdfs[i//N][j//N]
+        r,g,b = img[i][j]
+        out[i][j] = np.array([fR[r], fG[g], fB[b]])
+      
+      elif(i>(N-(N//2))):
+        fR, fG, fB = cdfs[cdf_m-1][j//N]
+        r,g,b = img[i][j]
+        out[i][j] = np.array([fR[r], fG[g], fB[b]])
+      
+      elif(j>(N-(N//2))):
+        fR, fG, fB = cdfs[i//N][cdf_n-1]
+        r,g,b = img[i][j]
+        out[i][j] = np.array([fR[r], fG[g], fB[b]])
+      
+      else:
+        fR1, fG1, fB1 = cdfs[i//N][j//N]
+        fR2, fG2, fB2 = cdfs[i//N][j//N + 1]
+        fR3, fG3, fB3 = cdfs[i//N + 1][j//N]
+        fR4, fG4, fB4 = cdfs[i//N + 1][j//N + 1]
+
+        r,g,b = img[i][j]
+        
+        r1,b1,g1 = fR1[r], fG1[g], fB1[b] 
+        r2,b2,g2 = fR2[r], fG2[g], fB2[b] 
+        r3,b3,g3 = fR3[r], fG3[g], fB3[b] 
+        r4,b4,g4 = fR4[r], fG4[g], fB4[b] 
+        
+        term_r = (((((i//N) + N - i)*((j//N) + N - j))/(N*N))*r4) + \
+            (((((i//N) + N - i)*((j//N) - j))/(N*N))*r3) + \
+            (((((i//N) - i)*((j//N) + N - j))/(N*N))*r2) + \
+            (((((i//N) - i)*((j//N) - j))/(N*N))*r1)
+        
+        term_g = (((((i//N) + N - i)*((j//N) + N - j))/(N*N))*g4) + \
+            (((((i//N) + N - i)*((j//N) - j))/(N*N))*g3) + \
+            (((((i//N) - i)*((j//N) + N - j))/(N*N))*g2) + \
+            (((((i//N) - i)*((j//N) - j))/(N*N))*g1)
+        
+        term_b = (((((i//N) + N - i)*((j//N) + N - j))/(N*N))*b4) + \
+            (((((i//N) + N - i)*((j//N) - j))/(N*N))*b3) + \
+            (((((i//N) - i)*((j//N) + N - j))/(N*N))*b2) + \
+            (((((i//N) - i)*((j//N) - j))/(N*N))*b1)            
+        
+        out[i][j] = np.array([term_r, term_g, term_b])
+        
+  image_titles=["Original Image", "Contrasted Image"]
+  
+  ax = plt.subplot(1,2,1)
+  im = ax.imshow(img, cmap='gray')
+  ax.set_title(image_titles[0])
+
+  ax = plt.subplot(1,2,2)
+  
+  im = ax.imshow(out, cmap='gray')
+  ax.set_title(image_titles[1])
+  
+  divider = make_axes_locatable(ax)
+  cax = divider.append_axes('right', size='5%', pad=0.05)
+  
+  plt.colorbar(im, cax= cax)
+  plt.tight_layout()
+
+
+def myExactCLAHE(path, N, threshold):
   if(isinstance(path, str)):
     img = extractImage(path)
   elif(isinstance(path, np.ndarray)):
@@ -113,11 +233,13 @@ def myCLAHE(path, N, threshold):
 
 
 if __name__ == '__main__':
-  path = os.path.join(os.path.dirname(__file__), '..', 'data', 'chestXray.png')
+  path = os.path.join(os.path.dirname(__file__), '..', 'data', 'barbara.png')
   print(path)
 
   start = time.time()
-  myCLAHE(path, 40, 0.02) # window size and threshold
-  print(time.time() - start)
 
+  # myBilinearCLAHE(path, 20, 0.02) # window size and threshold
+  myExactCLAHE(path, 64, 0.02) # window size and threshold
+  
+  print(time.time() - start)
   plt.show()
